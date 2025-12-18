bb332d84b197f8bb8884823be70e208419ff2024
toideptrai <nhaianh11223344@gmail.com>
2025-12-18 20:38:54 +0700

    sua giao dien

--- elder_app/android/app/src/main/kotlin/com/eldercare/elder_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.eldercare.elder_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ frontend/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'An Tâm',
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('An Tâm'),
+              const Text('Đăng nhập'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
